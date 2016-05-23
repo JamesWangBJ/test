787fc9c27a8203929c88e8c4fefb9c9df00be1f3
JamesWangBJ <Jm@wangjiamindeMBP.lan>
2016-05-23 17:17:40 +0800

    标题
    详情

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by 王佳敏 on 16/5/23.
+//  Copyright © 2016年 Jm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
